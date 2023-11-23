212ef6185d482db1e1c6e2881dd5b2b0e06fb514
joonyoung <joonsawon@gmail.com>
2023-11-23 22:01:17 +0900

    생활코딩 연습중..

--- myproject/스도쿠/urls.py
+++ myproject/스도쿠/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from 스도쿠 import views
 
+
 urlpatterns = [
-    path('', views.스도쿠)
+    path('', views.index),
+    path('create/', views.create),
+    path('read/<id>/', views.read)
 ]
--- myproject/스도쿠/views.py
+++ myproject/스도쿠/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render, HttpResponse
+import random
+
+topics = [{'id':1,'title':'Routing','body':'Routing is ..'},
+          {'id':2,'title':'View','body':'View is ..'},
+          {'id':3,'title':'Model','body':'Model is ..'}]
 
 def index(request):
-    return HttpResponse('Welcome!')
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}>{topic["title"]}</a></li>'
+
+    return HttpResponse(f'''
+    <h1>스도쿠</h1>
+    <ol>
+        {ol}
+    </ol>
+    <h2>Welcome</h2>
+    Hello, Django
+    ''')
+#return HttpResponse('<h1>Random</h1>'+str(random.random()))
 
 def 스도쿠(request):
     article = '''
-        <input type="int" name="title placeholder="배열사이즈(정수)">
+        <input type="int" name="size" placeholder="배열사이즈(정수)">
     '''
-    return HttpResponse('Welcome!')
+    return HttpResponse(article)
+
+def create(request):
+    return HttpResponse('Create')
+
+def read(request, id):
+    return HttpResponse('Read!'+id)
