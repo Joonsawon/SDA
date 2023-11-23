7079ee3bcdecc936e63e577dcd9bd2d7719b5ca3
joonyoung <joonsawon@gmail.com>
2023-11-23 21:15:48 +0900

    views.py에 스도쿠 함수 추가

--- myproject/스도쿠/urls.py
+++ myproject/스도쿠/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from 스도쿠 import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.스도쿠)
 ]
--- myproject/스도쿠/views.py
+++ myproject/스도쿠/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse('Welcome!')
+
+def 스도쿠(request):
+    article = '''
+        <input type="int" name="title placeholder="배열사이즈(정수)">
+    '''
+    return HttpResponse('Welcome!')
