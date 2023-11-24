a837947a62ab304934763636f25ae534c2957bd6
princebyun <princebyun@gmail.com>
2023-11-24 23:49:37 +0900

    로컬호스트  수용

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$)^#a8419sn$a%r(#+!gvz9wpqg@d-*-jisb4k@dff=t$_smtg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['150.230.252.97']
+ALLOWED_HOSTS = ['150.230.252.97','127.0.0.1']
 
 # Application definition
 
